48809d40906da7173f44d73baecc12b2e4daf42e
Kennedy Njeri <mistakenz123@gmail.com>
2019-05-25 23:47:16 +0300

    get absolute url remove from cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import item_list, products, checkout, HomeView, ItemDetailView, add_to_cart
+from .views import item_list, products, checkout, HomeView, ItemDetailView, add_to_cart, remove_from_cart
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='item_list'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('checkout/', checkout, name='checkout'),
-    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart')
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
 
 
 ]
