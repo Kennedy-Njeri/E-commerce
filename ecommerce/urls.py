33fb45a4831162717b2d4b040d294ff67fbe255b
Kennedy Njeri <mistakenz123@gmail.com>
2019-06-07 23:23:32 +0300

    remove single item func in views and temp

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,7 +6,8 @@ from .views import (item_list,
                     ItemDetailView,
                     add_to_cart,
                     remove_from_cart,
-                    OrderSummaryView
+                    OrderSummaryView,
+                    remove_single_item_from_cart,
                     )
 
 
@@ -16,7 +17,8 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('checkout/', checkout, name='checkout'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-single-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart')
 
 
 ]
