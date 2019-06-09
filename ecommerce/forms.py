9e5a48cc58e71227f58fe635f8d97b9d5b6b12bf
Kennedy Njeri <mistakenz123@gmail.com>
2019-06-09 23:51:35 +0300

    checkout form fix

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -18,4 +18,4 @@ class CheckoutForm(forms.Form):
     zip = forms.CharField()
     same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect(choices=PAYMENT_CHOICES))
+    payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
