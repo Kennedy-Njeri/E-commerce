04dc8c6ff3fac722ff4b3d4638fe190d8d70c260
Kennedy Njeri <mistakenz123@gmail.com>
2019-06-09 23:59:17 +0300

    placeholder fix

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -15,8 +15,10 @@ class CheckoutForm(forms.Form):
     street_address = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': '1234 Main St'
     }))
-    apartment_address = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country)')
+    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Apartment or suite'
+    }))
+    country = CountryField(blank_label='(select country)').formfield()
     zip = forms.CharField()
     same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
