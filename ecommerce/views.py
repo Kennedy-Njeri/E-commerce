8207cd8190bc7ea81e0b54a67ed283e9e559b97c
Kennedy Njeri <mistakenz123@gmail.com>
2019-06-17 23:49:57 +0300

    form cleaned data in views

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Item, OrderItem, Order
+from .models import Item, OrderItem, Order, BillingAddress
 from django.views.generic import ListView, DetailView, View
 from django.shortcuts import redirect
 from django.core.exceptions import ObjectDoesNotExist
@@ -75,6 +75,24 @@ class CheckoutView(View):
         #print(self.request.POST)
 
         if form.is_valid():
+
+            street_address = form.cleaned_data.get('street_address')
+            apartment_address = form.cleaned_data.get('apartment_address')
+            country = form.cleaned_data.get('country')
+            zip = form.cleaned_data.get('zip')
+
+            same_billing_address = form.cleaned_data.get(
+            'same_billing_address')
+            save_info = form.cleaned_data.get('save_info')
+            payment_option = form.cleaned_data.get('payment_option')
+            billing_address = BillingAddress(
+                user=self.request.user,
+                street_address=street_address,
+                apartment_address=apartment_address,
+                country=country,
+                zip=zip
+            )
+
 
             #print(form.cleaned_data)
 
