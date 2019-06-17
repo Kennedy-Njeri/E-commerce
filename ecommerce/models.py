7a8c16cc7f537945ad3b82c255ce3d97af6178cc
Kennedy Njeri <mistakenz123@gmail.com>
2019-06-17 23:39:43 +0300

    added billing address model

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.shortcuts import reverse
+from django_countries.fields import CountryField
 
 
 
@@ -105,5 +106,18 @@ class Order(models.Model):
             total += order_item.get_final_price()
 
         return total
+
+
+
+class BillingAddress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    street_address = models.CharField(max_length=100)
+    apartment_address = models.CharField(max_length=100)
+    countries = CountryField(multiple=True)
+    zip = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
+
 
 
