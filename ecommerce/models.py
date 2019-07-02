8f3f2bfed6843efee7546e532a0be9c519bf5746
Kennedy Njeri <mistakenz123@gmail.com>
2019-07-02 23:33:12 +0300

    clean up

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -122,6 +122,7 @@ class BillingAddress(models.Model):
 
 
 
+
 class Payment(models.Model):
     stripe_charge_id = models.CharField(max_length=50)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
